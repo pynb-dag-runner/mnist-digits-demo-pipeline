99ef8c25ab7837ccdff15801d2598c99bf00d936
Matias Dahl <matias.dahl@iki.fi>
2021-09-05 15:36:56 +0300

    read/write numpy + test

--- workspace/common/common/io.py
+++ workspace/common/common/io.py
@@ -1,9 +1,29 @@
+import os
 from pathlib import Path
 
+#
+import numpy as np
 
-def write_numpy(path: Path, v):
-    return 123
+
+def write_numpy(path: Path, numpy_obj):
+    """
+    Serialize and write a numpy array to a local file
+    """
+    assert path.suffix == ".numpy"
+
+    # Create local directory for file if it does not exist
+    os.makedirs(path.parent, exist_ok=True)
+
+    with open(path, "wb") as f:
+        np.save(f, numpy_obj, allow_pickle=False)
 
 
 def read_numpy(path: Path):
-    return 123
+    """
+    Read numpy array from a local file saved with write_numpy, see above
+    """
+    assert path.suffix == ".numpy"
+    assert path.is_file()
+
+    with open(path, "rb") as f:
+        return np.load(f)
--- workspace/common/tests/test_io.py
+++ workspace/common/tests/test_io.py
@@ -1,5 +1,19 @@
-from common.io import read_numpy
+from pathlib import Path
+
+#
+import numpy as np
+
+#
+from common.io import write_numpy, read_numpy
 
 
-def test_io():
-    assert read_numpy(1) == 123
+def test_numpy_read_write(tmp_path: Path):
+    filepath = tmp_path / "foo" / "bar" / "baz.numpy"
+
+    v1 = np.random.rand(1, 2, 3, 4)
+    assert v1.shape == (1, 2, 3, 4)
+    write_numpy(filepath, v1)
+
+    v2 = read_numpy(filepath)
+    assert v1.shape == v2.shape
+    assert (v1 == v2).all()
