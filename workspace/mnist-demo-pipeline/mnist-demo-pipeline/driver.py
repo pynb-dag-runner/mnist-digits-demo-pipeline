6ffa66dcdc4598786ca79879b0b21be18557f6d9
Matias Dahl <matias.dahl@iki.fi>
2021-09-18 14:12:49 +0300

    split dev and ci run environments (#5)

--- workspace/mnist-demo-pipeline/mnist-demo-pipeline/driver.py
+++ workspace/mnist-demo-pipeline/mnist-demo-pipeline/driver.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import uuid
+import uuid, datetime
 
 #
 from pynb_dag_runner.wrappers.runlog import Runlog
@@ -24,17 +24,25 @@ def get_args():
         type=str,
         help="local directory for data artefacts (normally written to a data lake)",
     )
+    parser.add_argument(
+        "--run_environment",
+        type=str,
+        choices=["ci", "dev"],
+        help="run environment for running pipeline",
+    )
 
     args = parser.parse_args()
 
     print("*** Command line parameters ***")
     print(f"  - runlogs_root     : {args.runlogs_root}")
     print(f"  - data_lake_root   : {args.data_lake_root}")
+    print(f"  - run_environment  : {args.run_environment}")
 
     return args
 
 
 args = get_args()
+start_time: str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 COMMON_PARAMETERS = {
     # data lake root is pipeline-scoped parameter
@@ -42,14 +50,25 @@ COMMON_PARAMETERS = {
 }
 
 
-def make_run_path(runlog: Runlog):
+def make_runlogs_root():
+    # If we are running in dev (eg automatic run in VS Code, or triggerd from cli)
+    # then put each pipeline run in a separate directory under runlogs_root.
+    if args.run_environment == "dev":
+        runlog_prefix = start_time
+    else:
+        runlog_prefix = ""
+
+    return Path(args.runlogs_root) / runlog_prefix
+
+
+def make_task_runlogs_root(runlog: Runlog):
     """
     Define organization of runlog-files from this pipeline
     """
     # extract filename of Jupytext notebook being run
     task_name = Path(runlog["notebook_path"]).name.replace(".py", "")
 
-    return Path(args.runlogs_root) / task_name / runlog["parameters.run.id"]
+    return make_runlogs_root() / task_name / runlog["parameters.run.id"]
 
 
 def make_notebook_task(
@@ -58,7 +77,7 @@ def make_notebook_task(
     jupytext_notebook = JupytextNotebook(notebook_path)
     return JupytextNotebookTask(
         jupytext_notebook,
-        get_run_path=make_run_path,
+        get_run_path=make_task_runlogs_root,
         n_max_retries=n_max_retries,
         timeout_s=timeout_s,
         task_id=str(uuid.uuid4()),
@@ -84,7 +103,7 @@ print("---- Running mnist-demo-pipeline ----")
 _ = flatten(run_tasks(tasks, task_dependencies))
 
 write_json(
-    Path(args.runlogs_root) / "task_dependencies.json",
+    make_runlogs_root() / "task_dependencies.json",
     get_task_dependencies(task_dependencies),
 )
 
