534e63bacc0215d346446a57d1a45bee1a92a432
Matias Dahl <matias.dahl@iki.fi>
2022-05-28 14:53:26 +0300

    revise how report is written

--- workspace/mnist-demo-pipeline/mnist-demo-pipeline/reporting.py
+++ workspace/mnist-demo-pipeline/mnist-demo-pipeline/reporting.py
@@ -13,19 +13,11 @@ def args():
         help="output path where OpenTelemetry spans have been expanded (tmp)",
     )
 
-    parser.add_argument(
-        "--github_step_summary_path",
-        type=Path,
-        help="filepath to write pipeline summary, see "
-        "https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#adding-a-job-summary",
-    )
-
     return parser.parse_args()
 
 
 print(f"--- Command line parameters")
-print(f"  - pipeline_outputs_path    : {args().pipeline_outputs_path}")
-print(f"  - github_step_summary_path : {args().github_step_summary_path}")
+print(f"  - pipeline_outputs_path         : {args().pipeline_outputs_path}")
 
 
 def make_markdown_report(pipeline_outputs_path: Path) -> str:
@@ -42,7 +34,7 @@ def make_markdown_report(pipeline_outputs_path: Path) -> str:
     return "\n".join(report_lines)
 
 
-args().github_step_summary_path.write_text(
+(args().pipeline_outputs_path / "pipeline_run_summary.md").write_text(
     make_markdown_report(args().pipeline_outputs_path)
 )
 
